cb31291447bd0cf8ffe672ea979278d43bfff393
Travis Carter <carter.travis94@gmail.com>
2019-08-16 08:01:38 -0600

    finish app

--- magic_eight_ball.rb
+++ magic_eight_ball.rb
@@ -23,53 +23,74 @@ class User
     "Definite NO!",
     "It seems unlikely."]
 
-    @random_answer = @answer.sample
+    @random_answer = @answer.shuffle.first
 
-        main_menu
+        menu_main
     end
     
 
-    def main_menu
+    def menu_main
         
     puts "--- MAGIC 8 BALL ---".colorize(:cyan)
     puts "1. Ask a question".colorize(:yellow)
     puts "Type 'quit' to Exit!".colorize(:yellow)
-    choice = gets.strip
-
-    case choice
+    @choice = gets.strip
+    choice_menu
+    end
+    def choice_menu
+    case @choice
     when "1"
-        puts "Ask the 8 Ball a question:"
+        puts "Ask the 8 Ball a question:".colorize(:yellow)
         gets
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".colorize(:cyan)
+        puts "Magic 8 Ball says: '#{@random_answer}'".colorize(:cyan)
+        puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".colorize(:cyan)
+        puts "1. Ask another question".colorize(:yellow)
+        puts "Type 'quit' to exit".colorize(:yellow)
         
-        puts @random_answer
-        puts "1. Ask another question"
-        puts "2. Exit"
-        choice_2 = gets.to_i
+        choice_2 = gets.strip
         
         case choice_2
-        when 1
-            main_menu
-        when 2
+        when "1"
+            menu_main
+        when "quit"
+            puts "Goodbye!".colorize(:cyan)
+        sleep (2)
             exit
         else
-            "Invalid Input".colorize(:red)
+            puts "Invalid Input".colorize(:cyan)
+            sleep (1)
+        
         end
 
         
 
     when "quit"
-        puts "Goodbye!"
+        puts "Goodbye!".colorize(:cyan)
+        sleep (2)
         exit
-    when "add_answers"
-
+    when "add_answer"
+        puts "Add Your Own Answer:".colorize(:cyan)
+        @add_answer = gets.strip
+        @answer.push(@add_answer)
+        puts "'#{@add_answer}' has been added!".colorize(:cyan)
+        menu_main
+    when "reset_answers"
+        puts "Answers have been reset".colorize(:cyan)
+        User.new
+        
+    when "print_answers"
+        puts @answer
+        menu_main
     else
         puts "Invalid Input".colorize(:red)
         sleep (1)
-        main_menu
+        menu_main
     end
   
 end
 end
+
 
 User.new
 
